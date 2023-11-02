c9aaf3a2b8333c10efc225062eaaf91f96348d85
Luu <130262301+Luudeus@users.noreply.github.com>
2023-11-02 01:32:25 -0300

    Fix test_file_management.py
    1. **Refined Test Structure:**
       - Organized the tests into separate files for clarity and maintainability.
       - Each file focuses on a particular aspect of functionality (e.g., file management, directory operations).
    
    2. **Improved Test Environment Setup and Teardown:**
       - Implemented a pytest fixture (`create_test_environment`) to handle setup and teardown seamlessly.
       - Removed the standalone `teardown_function`, delegating its responsibilities to the fixture.
    
    3. **Improved Configuration for pytest:**
       - Created a `pytest.ini` file to hold configuration, providing pytest with context about where to find the modules.
       - Adjusted the `PYTHONPATH` in the configuration file to ensure modules are correctly located during tests.
    
    4. **Refined Import Statements in Tests:**
       - Modified the import statements in test files to be compatible with the directory structure and pytest's configuration.
    
    5. **Optimized Temporary File and Directory Handling:**
       - Utilized the `tempfile` module to handle temporary directories and files more robustly and safely.

--- tests/test_file_management.py
+++ tests/test_file_management.py
@@ -2,9 +2,11 @@
 # This includes creating directories, moving files, and listing directory contents.
 
 from project import get_files_in_directory, create_folder_if_absent, move_file_to_folder
+import shutil
 import os
 import tempfile
 import pytest
+
 
 # Using pytest fixtures to setup a clean test environment before each test
 @pytest.fixture
@@ -14,15 +16,23 @@ def create_test_environment():
     destination_folder = tempfile.mkdtemp()
     test_file_name = "test_file.txt"
     test_file_path = os.path.join(source_folder, test_file_name)
+
     with open(test_file_path, "w") as f:
         f.write("Test content")
-    return source_folder, destination_folder, test_file_name
+
+    yield source_folder, destination_folder, test_file_name
+
+    # Teardown: Cleanup the created temporary directories and files
+    shutil.rmtree(source_folder)
+    shutil.rmtree(destination_folder)
+
 
 def test_get_files_in_directory_returns_list(create_test_environment):
     source_folder, _, _ = create_test_environment
     # Ensure that the function returns a list of files
     files = get_files_in_directory(source_folder)
     assert isinstance(files, list)
+
 
 def test_create_folder_if_absent_creates_folder():
     # Create a temporary directory path for testing folder creation
@@ -33,6 +43,7 @@ def test_create_folder_if_absent_creates_folder():
     assert os.path.isdir(temp_folder_path)
     os.rmdir(temp_folder_path)  # Clean up after test
 
+
 def test_move_file_to_folder_moves_file(create_test_environment):
     source_folder, destination_folder, test_file_name = create_test_environment
     # Execute the move operation
@@ -40,11 +51,3 @@ def test_move_file_to_folder_moves_file(create_test_environment):
     # Verify that the file has been moved from the source to the destination
     assert not os.path.exists(os.path.join(source_folder, test_file_name))
     assert os.path.exists(os.path.join(destination_folder, test_file_name))
-
-# Clean up the temporary directories and files after each test
-def teardown_function(function, create_test_environment):
-    source_folder, destination_folder, _ = create_test_environment
-    if os.path.isdir(source_folder):
-        os.rmdir(source_folder)
-    if os.path.isdir(destination_folder):
-        os.rmdir(destination_folder)
